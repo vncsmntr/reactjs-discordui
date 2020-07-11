08bb8b0cfd1406d1891bf36b4eb79481e42f61bd
Vinicius Monteiro <vncsmntr.work@gmail.com>
2020-07-11 12:44:54 -0300

    Add Component Layout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Grid } from './styles'
+ 
+
+const Layout: React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default Layout;
