a5ae698a5759ecfaf779db6173ea8fa8992ef6bd
Vinicius Monteiro <vncsmntr.work@gmail.com>
2020-07-11 13:51:52 -0300

    Create Server List Column

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Grid } from './styles'
- 
+import ServerList from '../ServerList';
 
 const Layout: React.FC = () => {
     return (
-        <div>
-
-        </div>
+        <Grid>
+            <ServerList />
+        </Grid>
     )
 };
 
