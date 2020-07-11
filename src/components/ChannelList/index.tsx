89a2ec5e4b91bf7173b8a52ceecab14c2ceb8e96
Vinicius Monteiro <vncsmntr.work@gmail.com>
2020-07-11 16:10:34 -0300

    Created and complete ChannelInfo

---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Title, ExpandIcon } from './styles'
+
+const ChannelList: React.FC = () => {
+    return (
+        <Container>
+
+        </Container>
+    )
+};
+
+export default ChannelList;
