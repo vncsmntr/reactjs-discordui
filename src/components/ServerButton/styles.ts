7c559056d40a4398386dff9474a16d08befeda13
Vinicius Monteiro <vncsmntr.work@gmail.com>
2020-07-11 14:05:32 -0300

    Completed ServerList Column

--- src/components/ServerButton/styles.ts
+++ src/components/ServerButton/styles.ts
@@ -6,18 +6,35 @@ export const Button = styled.button<Props>`
     display: flex;
     align-items: center;
     justify-content: center;
-
     flex-shrink: 0;
-
     width: 48px;
     height: 48px;
-
+    border-radius: 50%;
     margin-bottom: 8px;
-
     background-color: ${Props => 
         Props.isHome ? 'var(--rocketseat)' : 'var(--primary)'};
     
-    
     position: relative;
-    cursor: pointer; 
+    cursor: pointer;
+
+    > img {
+        width: 24px;
+        height: 24px;
+    }
+
+    &::before {
+
+    }
+
+    &::after {
+
+    }
+
+    transition: border-radius .2s, backgroud-color .2s;
+
+    &.active, 
+    &:hover {
+        border-radius: 16px;
+        background-color: ${props => props.isHome ? 'var (--rocketseat)' : 'var(--discord)'};
+    }
 `;
