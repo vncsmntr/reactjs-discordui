0843f6bad09e67a33c59ac8069aee0696d6802e1
Vinicius Monteiro <vncsmntr.work@gmail.com>
2020-07-12 13:28:24 -0300

    Completed ChannelButton

--- src/components/ChannelList/index.tsx
+++ src/components/ChannelList/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Container, Title, ExpandIcon } from './styles'
+import ChannelButton from '../ChannelButton'
+import { Container, Category, AddCategoryIcon } from './styles'
 
 const ChannelList: React.FC = () => {
     return (
         <Container>
-
+            <Category>
+                <span>Canais de Texto</span>
+                <AddCategoryIcon /> 
+            </Category>
+                <ChannelButton channelName="chat-livre" />
+                <ChannelButton channelName="Trabalho" />
+                <ChannelButton channelName="Diversão" />
+                <ChannelButton channelName="Variados" />
+                <ChannelButton channelName="Off-Topic" />
         </Container>
     )
 };
